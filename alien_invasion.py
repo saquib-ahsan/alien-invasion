bb87694440355d91ae13c8a0749a138826b6eab1
Saquib Ahsan <saquibahsan27@gmail.com>
2022-02-13 08:55:56 +0530

    shoot bullets - delete old bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,6 +29,13 @@ class AlienInvasion(object):
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            # get rid of bullets that have disappeared
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            # print(len(self.bullets))
+            
             self._update_screen()
 
     def _check_events(self):
