f028b652f6379b1d4c9321f657e91562d37beb4f
Saquib Ahsan <saquibahsan27@gmail.com>
2022-05-01 15:18:36 +0530

    end the game - identify when parts of the game should run

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,9 +37,12 @@ class AlienInvasion(object):
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+            
             self._update_screen()
 
     def _check_events(self):
