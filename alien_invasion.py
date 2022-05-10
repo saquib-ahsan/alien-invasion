7c666585b3266667cf03742154c333dd6c731ce0
Saquib Ahsan <saquibahsan27@gmail.com>
2022-05-10 17:38:17 +0530

    add the play button - reset the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -65,7 +65,17 @@ class AlienInvasion(object):
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
         if self.play_button.rect.collidepoint(mouse_pos):
+            # reset the game statistics
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _check_keydown_events(self, event):
         """Respond to keypresses"""
