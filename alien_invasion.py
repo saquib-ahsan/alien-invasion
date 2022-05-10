b33a6a04400dd1a62eeb5ef3edd125a877c239a4
Saquib Ahsan <saquibahsan27@gmail.com>
2022-05-10 17:43:15 +0530

    add the play button - deactivate the play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -64,7 +64,8 @@ class AlienInvasion(object):
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
             # reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
