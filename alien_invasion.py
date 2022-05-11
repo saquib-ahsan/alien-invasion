9fa7f8d4ecea130b06d2b96d0dd6e1f17aa50f4f
Saquib Ahsan <saquibahsan27@gmail.com>
2022-05-11 08:54:50 +0530

    score - make a scoreboard

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -25,8 +26,10 @@ class AlienInvasion(object):
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
-        # create an instance to store game statistics
+        # create an instance to store game statistics and
+        # create a scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -68,7 +71,7 @@ class AlienInvasion(object):
         if button_clicked and not self.stats.game_active:
             # reset the game settings
             self.settings.initialize_dynamic_settings()
-            
+
             # reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -224,6 +227,9 @@ class AlienInvasion(object):
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # draw the score information
+        self.sb.show_score()
 
         # draw the play button if the game is inactive
         if not self.stats.game_active:
