08b0de7f5317fb6b38ff50597e514eb9e583aa7c
Saquib Ahsan <saquibahsan27@gmail.com>
2022-05-11 09:06:15 +0530

    score - make sure to score all hits

--- alien_invasion.py
+++ alien_invasion.py
@@ -131,7 +131,8 @@ class AlienInvasion(object):
                      True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
 
         if not self.aliens:
